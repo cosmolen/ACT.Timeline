394b5bfd206012124dd58833cef6d3d23adc75bb
Len Kim <cosmolen@gmail.com>
2019-12-30 01:31:23 +0900

    update dependacy packages

--- FairyZeta.FF14.ACT.Timeline/Properties/AssemblyInfo.cs
+++ FairyZeta.FF14.ACT.Timeline/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("FairyZeta.FF14.ACT.Timeline")]
-[assembly: AssemblyDescription("ACT Plugin DLL, build by anoyetta")]
+[assembly: AssemblyDescription("ACT Plugin DLL")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FZ.Timeline")]
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("038a247c-bc24-4eea-af2e-43d5d36163a6")]
-[assembly: AssemblyVersion("0.0.7.4")]
-[assembly: AssemblyFileVersion("0.0.7.4")]
+[assembly: AssemblyVersion("0.0.7.5")]
+[assembly: AssemblyFileVersion("0.0.7.5")]
